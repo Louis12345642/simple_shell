b2e36ceff00b1e83282660142d9c882a07e3b746
Mubarak kual louis <mubaraklouis@gmail.com>
2023-11-16 02:02:02 +0200

    the problem was errors1

--- errors1.c
+++ errors1.c
@@ -6,33 +6,33 @@
  * Return: 0 if no numbers in string, converted number otherwise
  *       -1 on error
  */
-int _erratoi(char *k)
+int _erratoi(char *s)
 {
-	int j = 0;
-	unsigned long int rt = 0;
+	int i = 0;
+	unsigned long int result = 0;
 
-	if (*k == '+')
-		k++;  /* TODO: why does it work like that ? */
-	for (j = 0;  k[j] != '\0'; j++)
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-		if (k[j] >= '0' && k[j] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			rt *= 10;
-			rt += (k[j] - '0');
-			if (rt > INT_MAX)
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
 				return (-1);
 		}
 		else
 			return (-1);
 	}
-	return (rt);
+	return (result);
 }
 
 /**
- * print_error - prints the error sms
- * @info: params to be return
- * @estr: string conatain some errors
- * Return: 0 no erors return here some here 
+ * print_error - prints an error message
+ * @info: the parameter & return info struct
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
  *        -1 on error
  */
 void print_error(info_t *info, char *estr)
@@ -47,16 +47,16 @@ void print_error(info_t *info, char *estr)
 }
 
 /**
- * print_d - it print a numbere here 
+ * print_d - function prints a decimal (integer) number (base 10)
  * @input: the input
- * @fd: it descripts the file here 
+ * @fd: the filedescriptor to write to
  *
  * Return: number of characters printed
  */
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
-	int j, co = 0;
+	int i, count = 0;
 	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
@@ -65,55 +65,55 @@ int print_d(int input, int fd)
 	{
 		_abs_ = -input;
 		__putchar('-');
-		co++;
+		count++;
 	}
 	else
 		_abs_ = input;
 	current = _abs_;
-	for (j = 1000000000; i > 1; j /= 10)
+	for (i = 1000000000; i > 1; i /= 10)
 	{
-		if (_abs_ / j)
+		if (_abs_ / i)
 		{
-			__putchar('0' + current / j);
+			__putchar('0' + current / i);
 			count++;
 		}
-		current %= j;
+		current %= i;
 	}
 	__putchar('0' + current);
-	co++;
+	count++;
 
-	return (co);
+	return (count);
 }
 
 /**
- * convert_number - used for converting a function
- * @num: nums
- * @base: based
- * @flags: stings flags
+ * convert_number - converter function, a clone of itoa
+ * @num: number
+ * @base: base
+ * @flags: argument flags
  *
  * Return: string
  */
-char *convert_number(long int nm, int ba, int fl)
+char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
 	static char buffer[50];
 	char sign = 0;
 	char *ptr;
-	unsigned long n = nm;
+	unsigned long n = num;
 
-	if (!(fl & CONVERT_UNSIGNED) && nm < 0)
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
 	{
-		n = -nm;
+		n = -num;
 		sign = '-';
 
 	}
-	array = fl & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
 
 	do	{
-		*--ptr = array[n % ba];
-		n /= ba;
+		*--ptr = array[n % base];
+		n /= base;
 	} while (n != 0);
 
 	if (sign)
@@ -122,19 +122,19 @@ char *convert_number(long int nm, int ba, int fl)
 }
 
 /**
- * remove_comments - function to replace a something
- * @buf: string of the address ara here
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
  *
  * Return: Always 0;
  */
 void remove_comments(char *buf)
 {
-	int j;
+	int i;
 
-	for (j = 0; buf[j] != '\0'; j++)
-		if (buf[j] == '#' && (!j || buf[j - 1] == ' '))
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
-			buf[j] = '\0';
+			buf[i] = '\0';
 			break;
 		}
 }
