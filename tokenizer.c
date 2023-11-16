7c5413f2be68f102b0b61283c1cfc6eed0bfad4b
Mubarak kual louis <mubaraklouis@gmail.com>
2023-11-16 04:49:40 +0200

    work on second last

--- tokenizer.c
+++ tokenizer.c
@@ -52,11 +52,11 @@ char **strtow(char *str, char *d)
  * **strtow2 - separates a string into sent
  * @str: thee inputing strings
  * @d: the delimeter
- * Return: a find to an arrays of strings, or not on failing
+ * Return: a find to an arrays of strings, or not on fail
  */
 char **strtow2(char *str, char d)
 {
-	int i, h, k, m, numwos = 0;
+	int i, j, k, m, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
@@ -64,31 +64,31 @@ char **strtow2(char *str, char d)
 	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] != d && str[i + 1] == d) ||
 		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
-			numwos++;
-	if (numwos == 0)
+			numwords++;
+	if (numwords == 0)
 		return (NULL);
-	s = malloc((1 + numwos) * sizeof(char *));
+	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (i = 0, h = 0; h < numwos; h++)
+	for (i = 0, j = 0; j < numwords; j++)
 	{
 		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
 		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
-		s[h] = malloc((k + 1) * sizeof(char));
-		if (!s[h])
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
 		{
-			for (k = 0; k < h; k++)
+			for (k = 0; k < j; k++)
 				free(s[k]);
 			free(s);
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[h][m] = str[i++];
-		s[h][m] = 0;
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-	s[h] = NULL;
+	s[j] = NULL;
 	return (s);
 }
